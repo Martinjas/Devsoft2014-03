bfd05e14c205a5b2df6667edf65f59bf4943fcac
Martinjas <bydejastrzebski@gmail.com>
2014-04-22 11:07:08 -0300

    asd

--- Week 6/Exercises/ex6.4.rb
+++ Week 6/Exercises/ex6.4.rb
@@ -89,7 +89,7 @@ STR
 
 # 1) Capturar o horário, tema, e título de cada notícia. Em seguida, para cada
 # notícia, imprimi-la com o seguinte formato: "TEMA: TÍTULO (HORA)"
-about=str.match(([(.)*]))
+about=str.match(/([(.)*])/)
 puts "#{about}"
 # Seu código aqui...
 
